776f8e716327e94842f44db690d3a9367ba9058c
Ricardo0919 <rickisierra03@gmail.com>
2024-11-08 23:04:39 -0600

    Basic project settings

---
+++ src/components/HeroHome/index.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react';
+
+function Gestion() {
+
+    return (
+        <div className="text-black text-center text-4xl py-20">
+            VerdeRaíz
+        </div>
+    );
+}
+
+export default Gestion;
