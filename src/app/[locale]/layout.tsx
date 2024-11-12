d2690e0eeea25d72a705132f3d400e09a947d5df
Ricardo0919 <rickisierra03@gmail.com>
2024-11-12 00:56:34 -0600

    Translation feature

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,8 @@
 import { getMessages, unstable_setRequestLocale } from "next-intl/server";
 import { ReactNode } from "react";
-import { locales } from "../../navigation";
+import { locales } from "../../config";
 import { NextIntlClientProvider } from "next-intl";
-import "./globals.css";
+import "../globals.css";
 
 // Función para generar los metadatos dinámicamente según el locale
 export async function generateMetadata({
@@ -17,8 +17,6 @@ export async function generateMetadata({
         description: locale === "es" ? "Sitio web de Verderaiz" : "Verderaiz Website", // Descripción dinámica
     };
 }
-
-// Componente de Layout
 type Props = {
     children: ReactNode;
     params: { locale: string };
@@ -28,22 +26,14 @@ export function generateStaticParams() {
     return locales.map((locale) => ({ locale }));
 }
 
-export default async function LocaleLayout({
-                                               children,
-                                               params,
-                                           }: Props) {
-    const { locale } = await params;  // Esperar a que `params` se resuelva
+export default async function LocaleLayout({children, params:{locale}}: Props) {
 
-    // Configurar el idioma para la solicitud
-    unstable_setRequestLocale(locale);
-
-    // Obtener los mensajes de traducción basados en el locale
     const messages = await getMessages();
 
     return (
         <html className="h-full" lang={locale}>
         <body className="flex h-full flex-col">
-        <NextIntlClientProvider locale={locale} messages={messages}>
+        <NextIntlClientProvider messages={messages}>
             {children}
         </NextIntlClientProvider>
         </body>
