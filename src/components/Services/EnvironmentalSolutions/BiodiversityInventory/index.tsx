f81f86490bf2b53033602c78d7bfa00a193a365c
DrearyLand <drearyland22021@gmail.com>
2025-04-29 12:17:09 -0600

    Hotfix Environmental Solutions BG's

--- src/components/Services/EnvironmentalSolutions/BiodiversityInventory/index.tsx
+++ src/components/Services/EnvironmentalSolutions/BiodiversityInventory/index.tsx
@@ -10,38 +10,41 @@ function BiodiversityInventory() {
 
     return (
         <div className="bg-light-beige flex flex-col items-right justify-center relative">
-            <div className="flex flex-col justify-center items-stretch md:flex-row pb-12 xl:pb-20">
-                <div className="text-black mx-9 md:w-1/2 xl:ml-16">
-                    <p className="font-montserrat text-justify md:text-xs lg:text-base xl:text-xl">
-                        {t("description1")}
-                    </p>
-                    <p className="font-montserrat text-justify mt-2 md:text-xs lg:text-base xl:text-xl lg:mt-5">
-                        {t("description2")}
-                    </p>
-                    <ul className="font-montserrat text-justify mt-2 md:text-xs lg:text-base xl:text-xl lg:mt-8 list-disc pl-5 space-y-2">
-                        <li>{t("list1")}</li>
-                        <li>{t("list2")}</li>
-                    </ul>
-                </div>
+            <div className="z-10">
+                <div className="flex flex-col justify-center items-stretch md:flex-row pb-12 xl:pb-20">
+                    <div className="text-black mx-9 md:w-1/2 xl:ml-16">
+                        <p className="font-montserrat text-justify md:text-xs lg:text-base xl:text-xl">
+                            {t("description1")}
+                        </p>
+                        <p className="font-montserrat text-justify mt-2 md:text-xs lg:text-base xl:text-xl lg:mt-5">
+                            {t("description2")}
+                        </p>
+                        <ul className="font-montserrat text-justify mt-2 md:text-xs lg:text-base xl:text-xl lg:mt-8 list-disc pl-5 space-y-2">
+                            <li>{t("list1")}</li>
+                            <li>{t("list2")}</li>
+                        </ul>
+                    </div>
 
-                <div className="md:w-1/2 ml-9 flex items-stretch h-[220px] md:h-[140px] lg:h-[200px] xl:h-[246px] mt-9 md:mt-0">
-                    <div className="relative w-full h-full md:h-auto md:pt-9">
-                        <Image
-                            src={BioInventory}
-                            alt="Biodiversity Inventory"
-                            fill
-                            className="object-cover rounded-r-none rounded-l-[96px]"
-                        />
-                        <div className="absolute inset-0 flex items-center justify-center p-4 text-center">
-                            <p className="text-white text-3xl md:text-2xl lg:text-4xl font-cambay p-4 rounded-lg">
-                                {t("imageQuote")}
-                            </p>
+                    <div
+                        className="md:w-1/2 ml-9 flex items-stretch h-[220px] md:h-[120px] lg:h-[160px] xl:h-[180px] mt-9 md:mt-0">
+                        <div className="relative w-full h-full md:h-auto md:pt-9">
+                            <Image
+                                src={BioInventory}
+                                alt="Biodiversity Inventory"
+                                fill
+                                className="object-cover rounded-r-none rounded-l-[36px] sm:rounded-l-[28px] lg:rounded-l-[36px]"
+                            />
+                            <div className="absolute inset-0 flex items-center justify-center p-4 text-center">
+                                <p className="text-white text-3xl md:text-2xl lg:text-4xl font-cambay p-4 rounded-lg">
+                                    {t("imageQuote")}
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    );
+);
 }
 
-export default BiodiversityInventory;
+            export default BiodiversityInventory;
